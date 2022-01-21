4db7660456d98797b93c3ddea02d9c9eceb90590
Chin <cteeeri@gmail.com>
2022-01-21 00:31:40 -0600

    added templates, basic list view

--- beskarsrc/recipe/views.py
+++ beskarsrc/recipe/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 
+from recipe.models import Ingredient
 # Create your views here.
+
+
+class IngredientListView(generic.ListView):
+    model = Ingredient
+
+
+
