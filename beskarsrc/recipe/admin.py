1509714acb9d00bdbfc4d3eb528c9dfde0b38b1b
Chin <cteeeri@gmail.com>
2022-01-20 23:35:02 -0600

    basic model and admin works

--- beskarsrc/recipe/admin.py
+++ beskarsrc/recipe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from recipe.models import Ingredient
 # Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'freshnesslevel')
+
+admin.site.register(Ingredient, RecipeAdmin)
+
--- beskarsrc/recipe/models.py
+++ beskarsrc/recipe/models.py
@@ -4,21 +4,20 @@ from django.db import models
 # Create your models here.
 
 class FreshnessLevel(models.TextChoices):
-    FRESH = 'FRESH'
-    DRIED = 'DRIED'
-    FERMENTED = 'FERMENTED'
-
+    FRESH = 'Fresh'
+    DRIED = 'Dried'
+    FERMENTED = 'Fermented'
+    PICKLED = 'Pickled'
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
     freshnesslevel = models.CharField(max_length=20, choices=FreshnessLevel.choices, default=FreshnessLevel.FRESH)
     
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['name', 'freshnesslevel'], name='ingredient freshness level')
+            models.UniqueConstraint(fields=['name', 'freshnesslevel'], name='ingredient_freshness_level')
         ]
 
-
-
-
-
+    def __str__(self):
+        return f'{self.name} {self.freshnesslevel}'
+    
