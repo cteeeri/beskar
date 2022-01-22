4be182a44c1e395cb020489cffd74e8ee8e1a40d
Chin <cteeeri@gmail.com>
2022-01-21 23:43:49 -0600

    modified settings to use project level static folder and statisfiles folder for collectstatic

--- beskarsrc/beskar/settings.py
+++ beskarsrc/beskar/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 import os, environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = environ.Path(__file__) - 3
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# BASE_DIR = environ.Path(__file__) - 3
 
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -127,7 +127,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+# django go through all apps and collect static files to below locations
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
+
+# also look for static files in following location
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
